9efb8f2693ed0efd730407d50b9a2dd78d71f34c
Vitor Azevedo <vitor.azevedo@icloud.com>
2024-07-30 00:01:49 +0200

    added icons and updated css

---
+++ src/components/icons/arrow.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+    <path d="M7.661 22l15.19 15.19-2.851 2.81-20-20 20-20 2.851 2.81-15.19 15.19h32.339v4h-32.339z"></path>
+  </svg>
+</template>
---
+++ src/components/icons/menu.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+    <path d="M-0 33.258v-2.956h40v2.956h-40zM-0 21.478v-2.956h40v2.956h-40zM-0 9.698v-2.956h40v2.956h-40z"></path>
+  </svg>
+</template>
